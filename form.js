80c1586481aa1a1d942aa9e2248dcaa2dbc95c95
iauditor5s <44363411+iauditor5s@users.noreply.github.com>
2018-10-26 10:33:41 +0600

    Update form.js

--- form.js
+++ form.js
@@ -1,9 +1,9 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbw71mAUv7MJeRvw6k-7rXew3z5sIBnEslBg7h6xq5222lU-2XQ/exec'
+   const scriptURL = 'https://script.google.com/macros/s/AKfycbw71mAUv7MJeRvw6k-7rXew3z5sIBnEslBg7h6xq5222lU-2XQ/exec'
    const form = document.forms['submit-to-google-sheet']
-const loading = document.querySelector('.js-loading')
-    const successMessage = document.querySelector('.js-success-message')
-    const errorMessage = document.querySelector('.js-error-message')
-  form.addEventListener('submit', e => {
+   const loading = document.querySelector('.js-loading')
+   const successMessage = document.querySelector('.js-success-message')
+   const errorMessage = document.querySelector('.js-error-message')
+   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       .then(response => showSuccessMessage(response))
