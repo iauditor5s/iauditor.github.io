e2c72bd9c670806afe82c88c773788e66c422d32
iauditor5s <44363411+iauditor5s@users.noreply.github.com>
2018-10-31 15:35:12 +0600

    Update down.js

--- down.js
+++ down.js
@@ -1,6 +1,6 @@
 $("#btnExport").click(function (e) {
     $(this).attr({
         'download': "download.xls",
-            'href': 'data:application/csv;charset=utf-8,' + encodeURIComponent( $('#table').html())
+            'href': 'data:application/csv;charset=utf-8,' + encodeURIComponent( $('#box').html())
     })
 });
