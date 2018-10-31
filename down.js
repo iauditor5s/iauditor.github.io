c3c0700a29ba92da2122a875e27ac7e83ed34d36
iauditor5s <44363411+iauditor5s@users.noreply.github.com>
2018-10-31 19:19:29 +0600

    Update down.js

--- down.js
+++ down.js
@@ -1,5 +1,5 @@
 $('#Export').click(function () {
-        var csvFormattedDataTable = google.visualization.dataTableToCsv(data);
+        var csvFormattedDataTable = google.visualization.dataTableToCsv(table);
         var encodedUri = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csvFormattedDataTable);
         this.href = encodedUri;
         this.download = 'table-data.csv';
